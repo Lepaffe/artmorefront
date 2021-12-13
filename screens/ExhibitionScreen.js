e7ae3f8298874f84dd163ca333c89f36ea043ead
aynur <aynur.kaysoy@gmail.com>
2021-12-13 10:07:34 +0100

    front expo

--- screens/ExhibitionScreen.js
+++ screens/ExhibitionScreen.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { View, ScrollView, Text, StyleSheet, Dimensions } from 'react-native'
-import { CheckBox, ListItem, Avatar, LinearProgress } from 'react-native-elements'
+import { View, Text, StyleSheet, Dimensions, FlatList, TouchableOpacity } from 'react-native'
+import { ListItem, Avatar, LinearProgress } from 'react-native-elements'
 import { connect } from 'react-redux'
 import { REACT_APP_URL_BACKEND } from "@env";
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 
 import {
@@ -18,6 +19,19 @@ import {
 import { useFonts } from 'expo-font'
 import AppLoading from 'expo-app-loading'
 
+const listTab = [
+  {
+    name: "All"
+  },
+  {
+    name: "Near me"
+  },
+  {
+    name: "My exhibitions"
+  }
+]
+
+
 function ExhibitionScreen(props) {
 
   let [fontsLoaded] = useFonts({
@@ -30,78 +44,103 @@ function ExhibitionScreen(props) {
     Heebo_900Black
   })
 
-  const [checkedAllExpo, setCheckedAllExpo] = useState(true);
-  const [checkedMyExpo, setCheckedMyExpo] = useState(false);
-  const [checkedExpoNearMe, setCheckedExpoNearMe] = useState(false);
   const [listExpo, setListExpo] = useState([])
-
-  var dateFormat = function (date) {
-    var newDate = new Date(date)
-    var format = (newDate.getMonth() + 1) + "." + newDate.getDate() + "." + newDate.getFullYear()
-    return format;
-  };
-
-  const getAllExpoNearMe = async () => {
-    console.log("hello")
-    if (checkedExpoNearMe) {
-      setCheckedExpoNearMe(false)
-    } else {
-      setCheckedExpoNearMe(true), setCheckedAllExpo(false), setCheckedMyExpo(false)
-    }
-    var rawResponse = await fetch(`${REACT_APP_URL_BACKEND}/get-exhibitions/:token`);
-    var response = await rawResponse.json();
-    console.log("data near me", response)
-  }
+  const [status, setStatus] = useState('')
+  const [datalist, setDatalist] = useState(listExpo)
+  const [likedExpo, setLikedExpo] = useState('')
+  const [myExpoList, setMyExpoList] = useState([]);
+  const [userCity, setUserCity] = useState('')
+  const [colorIconAddExpo, setColorIconAddExpo] = useState("black")
 
   useEffect(() => {
     async function loadExpo() {
-      var rawResponse = await fetch(`${REACT_APP_URL_BACKEND}/get-exhibitions`);
+      var rawResponse = await fetch(`${REACT_APP_URL_BACKEND}/get-exhibitions/${props.token}`);
       var response = await rawResponse.json();
-      console.log(response.listExpoBack)
-
-      // var listExpoCopy = []
-
-      // for (var i = 0; i < response.data.length; i++) {
-      //   listExpoCopy.push({
-      //     img: response.data[i].fields.image,
-      //     title: response.data[i].fields.title,
-      //     city: response.data[i].fields.city,
-      //     place : response.data[i].fields.placename,
-      //     address : response.data[i].fields.address,
-      //     date_start: dateFormat(response.data[i].fields.date_start),
-      //     date_end: dateFormat(response.data[i].fields.date_end)
-      //   })
-
-      // if (response.data[i].fields.image == null) {
-      //   response.data[i].fields.image = '../assets/category/abstract.jpg';
-      // }
       setListExpo(response.listExpoBack);
+      setUserCity(response.userCity)
     }
-
-    //
     loadExpo();
   }, []);
 
   var exhibitionsList = <LinearProgress style={{ margin: 30, width: 300 }} color="rgba(213, 208, 205, 0.7)" />
   if (listExpo.length > 0) {
 
-    exhibitionsList = listExpo.map((expo, i) => (
-      <ListItem key={i} bottomDivider>
-        <Avatar style={{ width: 100, height: 160 }} source={{ uri: expo.img }} />
-        <ListItem.Content>
-          <ListItem.Title style={{ fontFamily: 'Heebo_400Regular' }}>{expo.title}</ListItem.Title>
-          <ListItem.Subtitle style={{ fontFamily: 'Heebo_300Light', marginVertical: 5 }}>{expo.place}</ListItem.Subtitle>
-          <ListItem.Subtitle style={{ fontFamily: 'Heebo_300Light', marginVertical: 5 }}>{expo.address}</ListItem.Subtitle>
-          <ListItem.Subtitle style={{ fontFamily: 'Heebo_400Regular', fontSize: 12 }}>From {expo.date_start} to {expo.date_end}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
-    ))
+    exhibitionsList = <FlatList
+      data={datalist}
+      keyExtractor={(index) => index.toString()}
+      renderItem={({ item, index }) => {
+        return <View>
+          <ListItem bottomDivider>
+            <Avatar style={{ width: 100, height: 160 }} source={{ uri: item.img }} />
+            <ListItem.Content>
+              <ListItem.Title style={{ fontFamily: 'Heebo_400Regular' }}>{item.title}</ListItem.Title>
+              <ListItem.Subtitle style={{ fontFamily: 'Heebo_300Light', marginVertical: 5 }}>{item.place}</ListItem.Subtitle>
+              <ListItem.Subtitle style={{ fontFamily: 'Heebo_300Light', marginVertical: 5 }}>{item.address}</ListItem.Subtitle>
+              <ListItem.Subtitle style={{ fontFamily: 'Heebo_400Regular', fontSize: 12 }}>From {item.date_start} to {item.date_end}</ListItem.Subtitle>
+              <View style={{ alignSelf: 'flex-end' }} >
+                <TouchableOpacity>
+                  <Icon style={{ marginEnd: 10 }}
+                    name="plus"
+                    size={20}
+                    color={colorIconAddExpo}
+                    contentStyle={{ margin: 20 }}
+                    onPress={() => addToMyExpo()}
+                  >
+                  </Icon>
+                </TouchableOpacity>
+              </View>
+            </ListItem.Content>
+          </ListItem>
+        </View >
+      }}
+    />
   }
 
   if (!fontsLoaded) {
     return <AppLoading />
   }
 
+  const setStatusFilter = status => {
+    if (status === 'All') {
+      setDatalist(listExpo)
+    }
+    else if (status === 'Near me') {
+      setDatalist([...listExpo.filter(e => e.city === userCity)])
+    }
+    else if (status === 'My exhibitions') {
+      setDatalist(myExpoList)
+    }
+    setStatus(status)
+  }
+
+
+  const addToMyExpo = async () => {
+    console.log('hello')
+    for (let i = 0; i < datalist.length; i++) {
+      console.log("datalist boucle :",datalist)
+    if (likedExpo == false) {
+      const data = await fetch(`${REACT_APP_URL_BACKEND}/add-exhibitions`, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `token=${props.token}&title=${datalist[i].title}&place=${datalist[i].place}&address=${datalist[i].address}&date_start=${datalist[i].date_start}&date_end=${datalist[i].date_end}`
+      });
+      const dataJSON = await data.json();
+      setColorIconAddExpo('orange');
+      // props.addArtist(props.selectedArtist._id)
+    } else {
+      const data = await fetch(`${REACT_APP_URL_BACKEND}/delete-exhibitions`, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `token=${props.token}&title=${datalist[i].title}`
+      });
+      const dataJSON = await data.json();
+      setColorLike('black');
+      // props.deleteArtist(props.selectedArtist._id)
+    }
+
+    setLikedExpo(!likedArtist);
+  }
+  }
   return (
 
     <View style={styles.container}>
@@ -109,48 +148,16 @@ function ExhibitionScreen(props) {
         <Text style={{ fontFamily: 'Heebo_300Light', borderBottomColor: "red", textAlign: "center", fontSize: 18, padding: 20 }}> Exhibitions </Text>
       </View>
       <View style={{ flexDirection: "row" }} >
-        <CheckBox
-          title='All'
-          checkedIcon='dot-circle-o'
-          uncheckedIcon='circle-o'
-          checked={checkedAllExpo}
-          checkedColor='rgba(38, 50, 56, 0.8)'
-          uncheckedColor='rgb(213, 208, 205)'
-          onPress={checkedAllExpo ? () => setCheckedAllExpo(false) : () => [setCheckedAllExpo(true), setCheckedExpoNearMe(false), setCheckedMyExpo(false)]}
-          containerStyle={styles.checkbox}
-          textStyle={{ fontSize: 10, color: 'black' }}
-        />
-
-        <CheckBox
-          title='Near me'
-          checkedIcon='dot-circle-o'
-          uncheckedIcon='circle-o'
-          checked={checkedExpoNearMe}
-          checkedColor='rgba(38, 50, 56, 0.8)'
-          uncheckedColor='rgb(213, 208, 205)'
-          onPress={() => {
-            getAllExpoNearMe()
-          }}
-          containerStyle={styles.checkbox}
-          textStyle={{ fontSize: 10, color: 'black' }}
-        />
-
-        <CheckBox
-          title='My exhibitions'
-          checkedIcon='dot-circle-o'
-          uncheckedIcon='circle-o'
-          checked={checkedMyExpo}
-          checkedColor='rgba(38, 50, 56, 0.8)'
-          uncheckedColor='rgb(213, 208, 205)'
-          onPress={checkedMyExpo ? () => setCheckedMyExpo(false) : () => [setCheckedMyExpo(true), setCheckedAllExpo(false), setCheckedExpoNearMe(false)]}
-          containerStyle={styles.checkbox}
-          textStyle={{ fontSize: 10, color: 'black' }}
-        />
+        {listTab.map(e => (
+          <TouchableOpacity style={[styles.btnTab, status === e.name && styles.btnTabActive]} onPress={() => setStatusFilter(e.name)}>
+            <Text style={styles.textTab}>{e.name}</Text>
+          </TouchableOpacity>
+        ))
+        }
       </View>
 
-      <ScrollView>
-        {exhibitionsList}
-      </ScrollView>
+      {exhibitionsList}
+
     </View>
 
   );
@@ -189,6 +196,24 @@ const styles = StyleSheet.create({
     fontSize: 15,
     marginTop: 5,
     fontFamily: 'Heebo_300Light'
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  btnTab: {
+    padding: 10,
+    justifyContent: 'center',
+    borderWidth: 0.5,
+    width: Dimensions.get('window').width / 3,
+    borderColor: '#D5D8DC'
+  },
+  textTab: {
+    textAlign: 'center'
+  },
+  btnTabActive: {
+    backgroundColor: '#82E0AA'
   },
 });
 
